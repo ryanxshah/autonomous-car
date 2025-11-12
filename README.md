53ba0a580c29e277b5e365ab2efa8d6789a970df
Ryan Shah <ryanshahx@gmail.com>
2025-11-11 17:05:35 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Autonomous Car Computer Vision/Deep Learning Project
+
+In this project, I trained a virtual car to drive around various tracks. I created three different models: A transformer planner, based on the Perceiver architecture, a convolutional neural network planner, and a multilayer perceptron planner.
+
+* Designed a transformer planner based on the Perceiver architecture to train a virtual car to drive around various tracks by predicting waypoints based on lane boundaries. Also designed a convolutional neural network planner and a multilayer perceptron planner to compare performance.
+* Implemented a custom training pipeline using PyTorch and fine-tuned hyperparameters to improve model accuracy and training eﬃciency.
+* Achieved 90% track coverage and 100% track completion rate with the most optimal model.
+
+
+
+## Watch the car drive! 🏎️
+
+You can see how the car drives by navigating to the [videos](https://github.com/ryanxshah/autonomous-car/tree/main/videos) folder and downloading the videos of interest.
+
+There are 2 videos for each model, the transformer planner and the multilayer perceptron planner. Each video corresponds to a different track.
---
+++ files/train_cnn.py
@@ -0,0 +1,108 @@
+
+
+import torch
+import argparse
+from pathlib import Path
+import torch.utils.tensorboard as tb
+from datetime import datetime
+from files.models import load_model, save_model
+from files.datasets.road_dataset import load_data
+from .metrics import PlannerMetric
+
+def train(
+        model_name="cnn_planner",
+        num_epochs=50,
+        lr=1e-3,
+        batch_size=128,
+        num_workers=2,
+        seed=0,
+        exp_dir="logs",
+        **kwargs
+):
+    
+    # set device
+    device = torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu")
+
+    # set seed
+    torch.manual_seed(seed)
+
+    # set logger
+    log_dir = Path(exp_dir) / f"{model_name}_{datetime.now().strftime('%m%d_%H%M%S')}"
+    logger = tb.SummaryWriter(log_dir)
+
+    # load model and set model in training mode
+    model = load_model(model_name, **kwargs)
+    model = model.to(device)
+    model.train()
+
+    # load data
+    train_data = load_data("drive_data/train", shuffle=True, batch_size=batch_size, num_workers=num_workers)
+    val_data = load_data("drive_data/val", shuffle=False)
+
+    # create optimizer and loss function
+    loss_func = torch.nn.L1Loss()
+    optimizer = torch.optim.AdamW(model.parameters(), lr=lr)
+
+    global_step = 0
+    
+    # metrics
+    train_planner_metric = PlannerMetric()
+    val_planner_metric = PlannerMetric()
+
+    for epoch in range(num_epochs):
+
+        model.train()
+        for batch in train_data:
+            image = batch["image"].to(device)
+            track_left = batch["track_left"].to(device)
+            track_right = batch["track_right"].to(device)
+            waypoints = batch["waypoints"].to(device)
+            waypoints_mask = batch["waypoints_mask"].to(device)
+
+            preds = model(image)
+
+            loss = loss_func(preds[waypoints_mask], waypoints[waypoints_mask])
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            train_planner_metric.add(preds, waypoints, waypoints_mask)
+
+            global_step += 1
+
+        with torch.inference_mode():
+
+            model.eval()
+            for batch in val_data:
+                image = batch["image"].to(device)
+                track_left = batch["track_left"].to(device)
+                track_right = batch["track_right"].to(device)
+                waypoints = batch["waypoints"].to(device)
+                waypoints_mask = batch["waypoints_mask"].to(device)
+                
+                preds = model(image)
+                val_planner_metric.add(preds, waypoints, waypoints_mask)
+
+        train_metrics = train_planner_metric.compute()
+        val_metrics = val_planner_metric.compute()
+
+
+    save_model(model)
+    torch.save(model.state_dict(), log_dir / f"{model_name}.th")
+    print(f"Model saved to {log_dir / f'{model_name}.th'}")
+
+    if __name__ == "__main__":
+        parser = argparse.ArgumentParser()
+
+        parser.add_argument("--exp_dir", type=str, default="logs")
+        parser.add_argument("--model_name", type=str, required=True)
+        parser.add_argument("--num_epoch", type=int, default=50)
+        parser.add_argument("--lr", type=float, default=1e-3)
+        parser.add_argument("--seed", type=int, default=2024)
+
+        # optional: additional model hyperparamters
+        # parser.add_argument("--num_layers", type=int, default=3)
+
+        # pass all arguments to train
+        train(**vars(parser.parse_args()))
